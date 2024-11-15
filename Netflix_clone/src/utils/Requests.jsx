fda61fd96af33d23d3b01b7ea467f12a9359c3fa
meseretali1234 <97925633+meseretali1234@users.noreply.github.com>
2024-11-15 10:50:47 -0500

    add header

--- Netflix_clone/src/utils/Requests.jsx
+++ Netflix_clone/src/utils/Requests.jsx
@@ -1,4 +1,5 @@
-const API_KEY=process.env.REACT_APP_API_KEY
+// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY ="ed07dfa24b3daab9d1eb35e99a5a6ee5"
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
@@ -8,7 +9,7 @@ const requests = {
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-    fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`
+    fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
 };
 
 export default requests;
